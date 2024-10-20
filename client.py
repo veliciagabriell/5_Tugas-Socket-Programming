8e5315788cac5bbe3eefef886500ef6110de0f0b
veliciagabriell <19623135@std.stei.itb.ac.id>
2024-10-20 23:23:11 +0700

    feat: TCP over UDP

--- client.py
+++ client.py
@@ -41,24 +41,54 @@ port = int(input("Port: "))  # Convert port to int
 
 pw = input("Insert password: ") #masukkan password
 
-
 while (pw != "roomchat"):
     print("Wrong password")
     pw = input("Insert password: ")
 
 name = input("Nickname: ")
 
-def receive():
+ack_receive = {}
+def receive(sock, chat_listbox):
+    global ack_received
     while True:
         try:
             message, _ = client.recvfrom(1024)
-            print(message.decode())
+            decoded_message = (message.decode())
+
+            #memisahkan nomor urut (seq number) dari pesan utama
+            #TCP over UDP melihat urutan
+            seq_num, msg = decoded_message.split(' ', 1)
+            seq_num = int(seq_num)
+
+            #send ACK ke sender
+            ack_message = f"ACK {seq_num}"
+            client.sendto(ack_message.encode('utf-8'),_)
+
         except:
             pass
 
 # Memulai thread untuk menerima pesan dari server
 t = threading.Thread(target=receive)
 t.start()
+
+def send (sock, ipaddr, port, msg_entry, username, seq_num):
+    msg = f" {seq_num} {username}: {msg_entry.get()}"
+
+    sock.sendto(message.encode('utf-8', (ipaddr, port)))
+
+    while True:
+        try:
+            ack, _ = sock.recvform(1024)
+            ack_message = ack.decode('utf-8')
+            ack_num = int(ack_message.split(' '[1]))
+
+            if ack_num==seq_num:
+                print(f"ACK {ack_num} received")
+                break
+        except:
+            print("Resending message...")
+            sock.sendto(message.encode('utf-8')), (ipaddr,port)
+
 
 # Mengirimkan pesan signup dengan nama pengguna
 client.sendto(f"SIGNUP_TAG: {name}".encode(), (ipaddr, port))

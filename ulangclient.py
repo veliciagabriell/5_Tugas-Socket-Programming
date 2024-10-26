736a85f87c44226c19b6bbea06db41f214c814ad
veliciagabriell <19623135@std.stei.itb.ac.id>
2024-10-26 20:14:08 +0700

    feat: gui

--- ulangclient.py
+++ ulangclient.py
@@ -1,5 +1,8 @@
 import socket
 import threading
+import tkinter
+import tkinter.scrolledtext
+from tkinter import simpledialog
 
 class ChatNode:
     def __init__(self, local_host="0.0.0.0", local_port=8081, remote_host="192.168.0.135", remote_port=8081, nickname=""):
@@ -9,6 +12,77 @@ class ChatNode:
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.bind(self.local_address)
         self.unique_name_confirmed = False
+
+        msg = tkinter.Tk()
+        msg.withdraw()
+
+        self.nickname = simpledialog.askstring("Nickname", "Enter your nickname", parent = msg)
+        self.gui_done = False
+        self.running = True
+        gui_thread = threading.Thread(target = self.gui_loop)
+        receive_thread = threading.Thread(target = self.receive)
+
+        gui_thread.start()
+        receive_thread.start()
+    
+    def gui_loop(self):
+        self.win = tkinter.Tk()
+        self.win.configure(bg="lightpink")
+
+        self.chat_label = tkinter.Label(self.win, text="Chat: ", bg="lightpink")
+        self.chat_label.config(font=("Arial", 12))
+        self.chat_label.pack(padx=20, pady=5)
+
+        self.text_area = tkinter.scrolledtext.ScrolledText(self.win)
+        self.text_area.pack(padx=20, pady=5)
+        self.text_area.config(state='disabled')
+
+        self.msg_label = tkinter.Label(self.win, text="Message: ", bg="lightpink")
+        self.msg_label.config(font=("Arial", 12))
+        self.msg_label.pack(padx=20, pady=5)
+        
+        self.input_area = tkinter.Text(self.win, height=3)
+        self.input_area.pack(padx=20, pady=5)
+
+        self.send_button = tkinter.Button(self.win, text="Send", command=self.write)
+        self.send_button.config(font = ("Arial",12))
+        self.send_button.pack(padx=20, pady=5)
+
+        self.gui_done = True
+        self.win.protocol("WM_DELETE_WINDOW", self.stop)
+        self.win.mainloop()
+
+
+    def write(self):
+        message = f"{self.nickname}: {self.input_area.get('1.0', 'end')}"
+        self.socket.send(message.encode())
+        self.input_area.delete('1.0', 'end')
+
+    def stop(self):
+        self.running = False
+        self.win.destroy()
+        self.socket.close()
+        exit(0)
+
+    def receive(self):
+        while self.running:
+            try:
+                message = self.socket.recvfrom(4096)
+                if message == 'NICK':
+                    self.socket.send(self.nickname.encode())
+                else:
+                    if self.gui_done:
+                        self.text_area.config(state = 'normal')
+                        self.text_area.insert('end', message)
+                        self.text_area.yview('end')
+                        self.text_area.config(state='disabled')
+            except ConnectionAbortedError:
+                break
+            except:
+                print("Error")
+                self.socket.close()
+                break
+
 
     def receive_messages(self):
         """Listen for incoming messages and print them."""
@@ -70,8 +144,6 @@ if __name__ == "__main__":
     while password != "lahkocak":
         print("Wrong password.")
         password =  input("Enter password: ")
-    
-    nickname = input("Enter your nickname: ")
 
-    chat_node = ChatNode(local_host, local_port, remote_host, remote_port, nickname)
-    chat_node.start()
+    client = ChatNode(local_host, local_port, remote_host, remote_port, nickname="")
+    nickname = input("Enter your nickname: ")
